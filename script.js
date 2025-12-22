73630a74ebf156d4bfcccf58c4741fb549875f92
shahzaibriaz69 <shahzaibriaz0023@gmail.com>
2025-12-22 19:12:52 +0500

    set achor tag and add hover and animation

---
+++ script.js
@@ -0,0 +1,25 @@
+const { animate, text, stagger } = anime;
+
+const { chars: chars1} =text.split('.home-professoin-1', { chars:true })
+const { chars: chars2} =text.split('.home-professoin-2', { chars:true })
+
+animate(chars1, {
+  y: [
+    { to: ['100%', '0%'] },
+    { to: '-100%', delay: 2000, ease: 'in(3)' }
+  ],
+  duration: 900,
+  ease: 'out(3)',
+  delay: stagger(80),
+  loop: true,
+});
+animate(chars2, {
+  y: [
+    { to: ['100%', '0%'] },
+    { to: '-100%', delay: 2000, ease: 'in(3)' }
+  ],
+  duration: 900,
+  ease: 'out(3)',
+  delay: stagger(80),
+  loop: true,
+});
